df9d1ef69116fcef50e3109de8fe44616b713c1e
YaniorMunguia <111203108+YaniorMunguia@users.noreply.github.com>
2023-04-24 19:38:39 -0600

    Login

---
+++ Web/Blazor/Servicios/LoginServicio.cs
@@ -0,0 +1,24 @@
+﻿using Blazor.Interfaces;
+using Datos.Interfaces;
+using Modelos;
+using Datos.Repositorios;
+
+
+namespace Blazor.Servicios
+{
+    public class LoginServicio : ILoginServicio
+    {
+        private readonly Config _config;
+        private ILoginRepositorio _loginRepositorio;
+
+        public LoginServicio(Config config)
+        {
+            _config = config;
+            _loginRepositorio = new LoginRepositorio(config.CadenaConexion);
+        }
+        public async Task<bool> ValidarUsuario(Login login)
+        {
+            return await _loginRepositorio.ValidarUsuario(login);
+        }
+    }
+}
