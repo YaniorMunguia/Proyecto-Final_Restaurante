7107438478241fecdd035d6da73d858c175c225b
20171034 <zoeccg12@gmail.com>
2023-04-27 21:09:01 -0600

    Nuevo Cambio
    Cambios de Clientes

--- Web/Blazor/Pages/MisClientes/Clientes.razor.cs
+++ Web/Blazor/Pages/MisClientes/Clientes.razor.cs
@@ -6,15 +6,15 @@ using Modelos;
 
 namespace Blazor.Pages.MisClientes
 {
-	public class Clientes
+	public partial class Clientes
 	{
 		[Inject] private IClienteServicio clienteServicio { get; set; }
 
 		private IEnumerable<Cliente> liste { get; set; }
 
-		/*protected override async Task OnInitializedAsync()
+		protected override async Task OnInitializedAsync()
 		{
 			liste = await clienteServicio.GetListaAsync();
-		}*/
+		}
 	}
 }
