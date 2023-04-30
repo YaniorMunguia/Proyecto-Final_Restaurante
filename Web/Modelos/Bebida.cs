1a445ec8701f265c0f0cbc802ffa913745c7075d
YaniorMunguia <111203108+YaniorMunguia@users.noreply.github.com>
2023-04-30 13:06:12 -0600

    Bebidas

---
+++ Web/Modelos/Bebida.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Bebida
+    {
+        [Required(ErrorMessage = "El código es obligatorio")]
+        public string Codigo { get; set; }
+        [Required(ErrorMessage = "El nombre de la bebida es obligatorio")]
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Precio { get; set; }
+        public bool EstaActivo { get; set; }
+
+
+        public Bebida()
+        {
+        }
+        public Bebida(string codigo, string descripcion, int existencia, decimal precio, bool estaActivo)
+        {
+            Codigo = codigo;
+            Descripcion = descripcion;
+            Existencia = existencia;
+            Precio = precio;
+            EstaActivo = estaActivo;
+        }
+    }
+
+}
