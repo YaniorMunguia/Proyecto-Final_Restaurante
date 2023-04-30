528266f1e87356c0ac1dab75bfa62d9aeb650660
AJuniorGarcia <124395333+AJuniorGarcia@users.noreply.github.com>
2023-04-30 07:16:57 +0200

    Producto
    nuevo cambios del menu productos

---
+++ Web/Blazor/Servicios/ProductoServicio.cs
@@ -0,0 +1,44 @@
+﻿using Blazor.Interfaces;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using Modelos;
+
+namespace Blazor.Servicios
+{
+    public class ProductoServicio : IProductoServicio
+    {
+        private readonly Config _config;
+        private IProductoRepositorio _productoRepositorio;
+
+        public ProductoServicio(Config config)
+        {
+            _config = config;
+            _productoRepositorio = new ProductoRepositorio(config.CadenaConexion);
+        }
+
+        public async Task<bool> Actualizar(Producto producto)
+        {
+            return await _productoRepositorio.Actualizar(producto);
+        }
+
+        public async Task<bool> Eliminar(string codigo)
+        {
+            return await _productoRepositorio.Eliminar(codigo);
+        }
+
+        public async Task<IEnumerable<Producto>> GetLista()
+        {
+            return await _productoRepositorio.GetLista();
+        }
+
+        public async Task<Producto> GetPorCodigo(string codigo)
+        {
+            return await _productoRepositorio.GetPorCodigo(codigo);
+        }
+
+        public async Task<bool> Nuevo(Producto producto)
+        {
+            return await _productoRepositorio.Nuevo(producto);
+        }
+    }
+}
